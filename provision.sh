434f48479a18c328a8fb6c77350e44e76d06064c
Roger Liang <roger@merlingroup.co>
2017-11-09 07:20:43 +1100

    update permission

--- provision.sh
+++ provision.sh
@@ -8,7 +8,7 @@ pip install mysql-python
 # SSH keys
 cp /home/vagrant/sync/ssh_keys/* /home/vagrant/.ssh/
 chown vagrant:vagrant /home/vagrant/.ssh/*
-chmod /home/vagrant/.ssh/*
+chmod 400 /home/vagrant/.ssh/*
 
 # Provision
 runuser -l vagrant -c 'cd /home/vagrant/sync/ansible/ && ansible-playbook -v -i hosts site.yaml' | sed 's/\\n/\n/g'
